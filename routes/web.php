6b7859cbeb8e065e957c2bccab728107cd51c3a8
= <tnylea@gmail.com>
2021-08-03 11:42:12 -0400

    Adding update

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    echo 'Hey Ma 👋, I just got deployed from Deployer! RAD!';
+    echo 'Hey Ma 👋, I just got deployed from Deployer! RAD! Yo';
 });
