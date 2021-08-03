a3d28efdd75eab114c79ccdbc7cfbb9d247a498d
= <tnylea@gmail.com>
2021-08-03 10:43:23 -0400

    adding updated routes

--- deploy.php
+++ deploy.php
@@ -7,7 +7,7 @@ require 'recipe/laravel.php';
 set('application', 'my_project');
 
 // Project repository
-set('repository', 'https://github.com/thedevdojo/laravel-cloudways-deployer.git');
+set('repository', 'git@github.com:thedevdojo/laravel-cloudways-deployer.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true);
